d1578f955361aecbde7d1021213016259ed39ce8
GregMarshall999 <gregory.marshall999@gmail.com>
2024-10-14 16:43:13 +0200

    ajout vuex store

--- src/App.vue
+++ src/App.vue
@@ -6,13 +6,4 @@
 <script setup>
 import ProductLister from './components/ProductLister.vue';
 import ProductViewer from './components/ProductViewer.vue';
-import { ref } from 'vue';
-
-const products = ref([
-  { name: 'Bananes', price: 2 },
-  { name: 'Pommes', price: 1 },
-  { name: 'Salade', price: 3 },
-  { name: 'Abricots', price: 2 }
-]);
-
 </script>
---
+++ src/store/index.js
@@ -0,0 +1,16 @@
+const { createStore } = require("vuex");
+
+const state = {
+    products: [
+        { name: 'Bananes', price: 2 },
+        { name: 'Pommes', price: 1 },
+        { name: 'Salade', price: 3 },
+        { name: 'Abricots', price: 2 }
+    ]
+};
+
+const store = createStore({
+    state
+});
+
+export default store;
