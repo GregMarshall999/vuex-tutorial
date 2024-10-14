ef999760c7b572b6c9f99bf6eacbd5ac56902211
GregMarshall999 <gregory.marshall999@gmail.com>
2024-10-14 16:35:21 +0200

    create structure

---
+++ src/components/ProductLister.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="lister">
+        <h2>Liste de produits</h2>
+
+        <ul>
+            <li v-for="product in productList">
+                <span class="name">{{ product.name }}</span>
+                <span class="price">{{ product.price }}€</span>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script setup>
+
+const props = defineProps({
+    productList: {
+        type: Array, 
+        required: true
+    }
+});
+
+</script>
+
+<style>
+
+.lister {
+    background: #6b662a;
+    box-shadow: 1px 2px 3px rgba(0,0,0,0.2);
+    margin-bottom: 30px;
+    padding: 10px 20px;
+    color: black;
+
+    ul {
+        padding: 0;
+
+        li{
+            display: inline-block;
+            margin-right: 10px;
+            margin-top: 10px;
+            padding: 20px;
+            background: rgba(255, 255, 255, 0.432);
+        }
+
+        .price{
+            font-weight: bold;
+            color: #e8260c;
+            margin-left: 4px;
+        }
+    }
+}
+
+</style>
---
+++ src/components/ProductViewer.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="viewer">
+        <h2>Produits disponible</h2>
+
+        <ul>
+            <li v-for="product in products">
+                <span class="name">{{ product.name }}</span>
+                <span class="price">{{ product.price }}€</span>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script setup>
+
+const props = defineProps({
+    products: {
+        type: Array, 
+        required: true
+    }
+});
+
+</script>
+
+<style scoped>
+
+.viewer {
+    background: #0c3772c2;
+    box-shadow: 1px 2px 3px rgba(0,0,0,0.2);
+    margin-bottom: 30px;
+    padding: 10px 20px;
+    color: white;
+
+    ul{
+        padding: 0;
+        list-style-type: none;
+
+        li{
+            margin-right: 10px;
+            margin-top: 10px;
+            padding: 20px;
+            background: rgba(255, 255, 255, 0.151);
+
+            .price{
+                font-weight: bold;
+                color: #bf97db;
+                display: block;
+            }
+        }
+    }
+}
+
+</style>
