ea7df8c650a119f66cb6fa3a97593c029278e6d4
GregMarshall999 <gregory.marshall999@gmail.com>
2024-10-14 17:21:29 +0200

    bonne pratique

--- src/components/ProductViewer.vue
+++ src/components/ProductViewer.vue
@@ -18,31 +18,7 @@ import { useStore } from 'vuex';
 const store = useStore();
 
 const products = computed(() => store.state.products);
-const viewSoldes = computed(() => {
-    var soldes = store.state.products.map(p => {
-        return {
-            name: `**${p.name}**`,
-            price: parseHalfPrice(p.price)
-        }
-    });
-    return soldes;
-});
-
-const parseHalfPrice = price => {
-    var hp = price / 2;
-
-    if(hp % 1 != 0) {
-        var precision = hp.toPrecision(3)
-
-        if(precision.split('.')[1].length > 2) {
-            return precision.substring(0, precision.split('.')[0].length + 3); //2 + le '.'
-        }
-        
-        return precision.toString();
-    }
-
-    return hp;
-};
+const viewSoldes = computed(() => store.getters.saleProducts);
 
 </script>
 
--- src/store/index.js
+++ src/store/index.js
@@ -9,8 +9,37 @@ const state = {
     ]
 };
 
+const getters = {
+    saleProducts: state => {
+        var soldes = state.products.map(p => {
+            return {
+                name: `**${p.name}**`,
+                price: parseHalfPrice(p.price)
+            }
+        });
+        return soldes;
+    }
+};
+
+const parseHalfPrice = price => {
+    var hp = price / 2;
+
+    if(hp % 1 != 0) {
+        var precision = hp.toPrecision(3)
+
+        if(precision.split('.')[1].length > 2) {
+            return precision.substring(0, precision.split('.')[0].length + 3); //2 + le '.'
+        }
+        
+        return precision.toString();
+    }
+
+    return hp;
+};
+
 const store = createStore({
-    state
+    state, 
+    getters
 });
 
 export default store;
